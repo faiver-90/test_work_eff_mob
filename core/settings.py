30def1186754a13396affe842a8be3af2aef930f
Vladimir Malashkin <faiver90@gmail.com>
2025-03-27 19:36:33 +0200

    create debug in .env

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m*j_ziw@bv52mt_ju+*mr6eg%sr06_e3%&&q+07p)5_=1l4a68'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
